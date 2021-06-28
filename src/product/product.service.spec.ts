154734f4e9f48e405bbc4d4be827415e95b725e3
Tuan Hoang <tuan@myzeller.com>
2021-06-28 09:49:26 +0700

    restruct tests

---
+++ src/product/product.service.spec.ts
@@ -0,0 +1,13 @@
+
+
+describe('Product Service', () => {
+  beforeEach(async () => {});
+
+  it('Cannot create if product is null', () => {});
+
+  it('Create a product success', () => {});
+
+  it('Cannot Delete a product if id is not found', () => {});
+
+  it('Cannot Delete a success', () => {});
+});
